6828587d6ca3dfd83ef3c24911b97e79259f34da
tanaxis@mail.ru <tanaxis@mail.ru>
2026-02-17 17:26:18 +0300

    пушу загрузку проекта

---
+++ levkulich/levkulich/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace levkulich
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
